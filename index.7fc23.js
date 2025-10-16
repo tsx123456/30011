875d24f0cb6c9902c81b62ab37e1951b597385d7
上帝的梦 <1597691532@qq.com>
2025-10-16 14:45:57 +0800

    1

--- index.7fc23.js
+++ index.7fc23.js
@@ -1,4 +1,4 @@
-System.register(["./application.8c7b3.js"], function (_export, _context) {
+System.register(["./application.19f91.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
